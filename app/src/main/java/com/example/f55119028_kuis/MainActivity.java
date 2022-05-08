dfd873a8c04023a2da20ff6f1fdcf76eb8d515e8
Maulana <maulanaadnan001@gmail.com>
2022-05-08 17:54:12 +0800

    Initial commit

--- app/src/main/java/com/example/f55119028_kuis/MainActivity.java
+++ app/src/main/java/com/example/f55119028_kuis/MainActivity.java
@@ -2,21 +2,16 @@ package com.example.f55119028_kuis;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.TypedArray;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
 import com.example.f55119028_kuis.databinding.ActivityMainBinding;
-import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private String[] dataName;
     private String[] dataDescription;
     private TypedArray dataPhoto;
-    private HeroAdapter adapter;
-    private ArrayList<Hero> heroes;
 
     private ActivityMainBinding binding;
 
@@ -26,39 +21,18 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 //        setContentView(R.layout.activity_main);
-        adapter = new HeroAdapter(this);
 
-        ListView listView = findViewById(R.id.lv_list);
-        listView.setAdapter(adapter);
-
-        prepare();
-        addItem();
-
-        binding.lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this,"Anda Mengklik: " + heroes.get(i).getName(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-    }
-
-    private void prepare() {
-        dataName = getResources().getStringArray(R.array.data_name);
-        dataDescription = getResources().getStringArray(R.array.data_description);
-        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
-    }
-
-    private void addItem() {
-        heroes = new ArrayList<>();
-
-        for (int i = 0; i < dataName.length; i++) {
-            Hero hero = new Hero();
-            hero.setPhoto(dataPhoto.getResourceId(i, -1));
-            hero.setName(dataName[i]);
-            hero.setDescription(dataDescription[i]);
-            heroes.add(hero);
-        }
-        adapter.setHeroes(heroes);
+        // Data Ahmad Dahlan
+        binding.imgPhoto1.setImageBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.ahmad_dahlan));
+        binding.txtName1.setText("Ahmad Dahlan");
+        binding.txtDescription1.setText("Salah seorang ulama dan khatib terkemuka di Masjid Besar Kasultanan Yogyakarta pada masa itu, ");
+        // Data Ahmad Yani
+        binding.imgPhoto2.setImageBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.ahmad_yani));
+        binding.txtName2.setText("Ahmad Yani");
+        binding.txtDescription2.setText("Jenderal TNI Anumerta Ahmad Yani (juga dieja Achmad Yani; lahir di Purworejo, Jawa Tengah, 19 Juni 1922");
+        // Data Maulana Adnan Khalid
+        binding.imgPhoto3.setImageBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.maul));
+        binding.txtName3.setText("Maulana Adnan Khalid");
+        binding.txtDescription3.setText("Hello!.. Nama Saya Maulana Adnan Khalid, Stambuk F55119028.. Saya tinggal di jalan lorong gelatik No.23 Palu");
     }
 }
